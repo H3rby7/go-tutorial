374d10f4b0a575b6f04fb9688cd792364bb6c83b
EXXETA-DE\joka <johannes.kahlhoefer@EXXETA.com>
2019-09-29 13:37:32 +0200

    comments

--- variadic/variadic.go
+++ variadic/variadic.go
@@ -7,19 +7,24 @@ func main() {
 		fmt.Println("Sum of 4,89,1 is", addUp(4, 89, 1))
 		mySlice := []int{69, 42, 1337}
 		// Note that we can pass the slice to a variadic function by appending '...'
-		// When we do this, our slice will be used as argument to the function. So no new slice is created.
-		// Any modification we make to the slice inside the function will affect the original slice.
 		fmt.Println(mySlice, "added up is:", addUp(0, mySlice...))
 	}
 	fmt.Println("\n-----------------------------------")
 	{
+		// When we do this, our slice will be used as argument to the function. So no new slice is created.
+		// Any modification we make to the slice inside the function will affect the original slice.
 		texts := []string{"Go", "is", "King"}
-		result := sliceFun(texts...)
-		fmt.Println(result)
+		sliceFun(texts...)
+		// Prints Joka is King
+		fmt.Println(texts)
+		texts2 := []string{"Go", "is", "King"}
+		sliceFun(texts2[0], texts2[1], texts2[2])
+		// Prints Go is King
+		fmt.Println(texts2)
 	}
 }
 
-func addUp(a int, b ... int) int {
+func addUp(a int, b ...int) int {
 	fmt.Printf("type of b is %T\n", b)
 	sum := a
 	for _, v := range b {
@@ -28,7 +33,7 @@ func addUp(a int, b ... int) int {
 	return sum
 }
 
-func sliceFun(input ... string) []string {
+func sliceFun(input ...string) []string {
 	input[0] = "Joka"
 	return input
 }
